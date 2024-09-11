e424bef5b131c9a4788a07bb2751c100a282a5e4
nathalymonge <nathaly.monge@ucas-edu.net>
2024-09-11 03:41:12 +0000

    finished variablepractice.c

--- Variables/VariablePractice.c
+++ Variables/VariablePractice.c
@@ -2,6 +2,24 @@
 
 int main(void){
     char name[] = "Nathaly Monge";
-    printf("%s", name);
+    printf("%s\n", name);
+    char lowNumber[] = "8";
+    printf("%s\n", lowNumber );
+    char highNumber[] = "108";
+    printf("%s\n", highNumber);
+    char breakfast[] = "cereal";
+    printf("%s\n", breakfast);
+    char favoriteColor[] = "red";
+    printf("%s\n", favoriteColor);
+    char school[] = "UCAS";
+    printf("%s\n", school);
+    char year[] = "2024";
+    printf("%s\n", year); 
+    char eyeColor[] = "brown";
+    printf("%s\n", eyeColor);
+    char age[] = "16";
+    printf("%s\n", age);
+    char favoriteSubject[] = "reading";
+    printf("%s\n", favoriteSubject);
     return 0;
 }
