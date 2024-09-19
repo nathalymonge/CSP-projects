ba3bc50bb3a5b8801ee22b86574e8ba8c93bd105
nathalymonge <nathaly.monge@ucas-edu.net>
2024-09-19 19:55:21 +0000

    deleted the intergers

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -1,25 +1,6 @@
 #include<stdio.h>
 
 int main(void){
-    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q;
-    a = 24;
-    b = 7;
-    c = 8;
-    d = 4;
-    e = 6;
-    f = 18;
-    g = 3;
-    h = 2;
-    i = 5;
-    j = 12;
-    k = 72;
-    l = 9;
-    m = 17;
-    n = 483;
-    o = 1;
-    p = 15;
-    q= 22;
-   scanf("%f", &a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q);
-   printf(num-numTwo/numThree*numFour+numFive)
+    printf(7-24/8*4+6);
     return 0;
 }
