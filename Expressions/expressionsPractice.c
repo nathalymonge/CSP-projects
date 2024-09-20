d9dab0b7414fd141f2967faf095ffbaa701ff936
nathalymonge <nathaly.monge@ucas-edu.net>
2024-09-20 07:25:07 +0000

    finished expressionsPractice.c

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -1,6 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(void){
-    printf(7-24/8*4+6);
+    int num = 7-24/8*4+6;
+    printf("%d\n", num);
+    int numone = 18/3-7+2*5;
+    printf("%d\n", numone);
+    int numtwo = 6*4/12+72/8-9;
+    printf("%d\n", numtwo);
+    int numthree = (17-6*2)+4/3;
+    printf("%d\n", numthree);
+    int numfour = -2*(1*4-2/2)+(6+2-3);
+    printf("%d\n", numfour);
+    int numfive = -1*((3-4*7)/5)-2*24/6;
+    printf("%d\n", numfive);
+    int numsix = (3*pow(5,2)/15)-(5-pow(2,2));
+    printf("%d\n", numsix);
+    int numseven = (pow(1,4)*pow(2,2)+pow(3,3)-pow(2,5)/4); 
+    printf("%d\n", numseven);
+    int numeight = (pow(22/2-2*5,2)+pow(4-6/6,2));
+    printf("%d\n", numeight);
     return 0;
 }
