c16c3ba71a95f828c7148305f2f37aedd263225b
nathalymonge <nathaly.monge@ucas-edu.net>
2024-10-01 20:03:52 +0000

    started on updatebudget.py

---
+++ Functions/updatebudget.py
@@ -0,0 +1,26 @@
+print("Hello, and welcome to your financial calculator!\n")
+income = int(input("What is your monthly income?: "))
+rent = int(input("What is your monthly rent: "))
+utilities = int(input("What is your monthly utilities: "))
+groceries = int(input("What is your monthly groceries: "))
+transportation = int(input("What does your monthly transportation cost: "))
+expenses = rent + utilities + transportation + groceries
+savings = income *.2
+total = income - savings - expenses
+print("Your monthly income is $",income)
+print("Your monthly expenses are $",expenses)
+print("Your monthly savings are $",savings)
+print("You have $",total, "left to spend")
+
+
+def percent(type,amount):
+    per = amount/income *100
+
+    return(f"Your {type} is {per}% of income.")
+
+print(percent("rent", rent))
+print(percent("utilities", utilities))
+print(percent("groceries", groceries))
+print(percent("transportation", transportation))
+print(percent("savings", savings))
+print(percent("expenses", expenses))
---
+++ Functions/updatehello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+void hello(char name[])
+
+    char name[] = "Randy";
+    char nname[] = "Jessica";
+
+int main(void){
+   printf("Hello %s\n", name);
+    return 0;
+
+}
