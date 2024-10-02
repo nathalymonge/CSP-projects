b89d2bc7dc4f70fbc2f39d1d90358fccb080a5a4
nathalymonge <nathaly.monge@ucas-edu.net>
2024-10-02 20:39:51 +0000

    finished updatehello.c

--- Functions/updatehello.c
+++ Functions/updatehello.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
+ char aname[] = "Randy";
+ char bname[] = "Jessica";
+ char cname[] = "Eva";
+ char dname[] = "Nathaly";
+ char ename[] = "Israel";
 
-void hello(char name[])
+void hello(char name[]){
+    printf("Hello %s\n", name);
+}
 
-    char name = "Randy";
-    char nname = "Jessica";
-
+   
 int main(void){
-   printf("Hello %s\n", name);
+   hello("Randy");
+   hello("Jessica");
+   hello("Eva");
+   hello("Nathaly");
+   hello("Israel");
     return 0;
 
 }
