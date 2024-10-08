41db2068185891ec83014d91d1ed251b2ce5fc56
nathalymonge <nathaly.monge@ucas-edu.net>
2024-10-08 07:21:51 +0000

    finished old.py

---
+++ Conditionals/old.py
@@ -0,0 +1,11 @@
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+elif age >= 16:
+    print("You are old enough to drive!")
+elif age >= 15:
+    print("You are old enough to get a learners permit!")
+elif age >= 5:
+    print("You are old enough to go to school!")
+else:
+    print("You're too young to go to school.")
